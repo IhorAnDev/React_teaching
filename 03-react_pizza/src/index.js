d62cb92da3d65fd1f73421358bbf9dc240f1a6dd
IhorAnDev <ihorandev@gmail.com>
2023-10-02 08:13:36 +0300

    refactoring

--- 03-react_pizza/src/index.js
+++ 03-react_pizza/src/index.js
@@ -115,9 +115,11 @@ function Pizza(props) {
     return (
         <li className="pizza">
             <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}/>
-            <h3>{props.pizzaObj.name}</h3>
-            <p>{props.pizzaObj.ingredients}</p>
-            <span>{props.pizzaObj.price + 3}</span>
+            <div>
+                <h3>{props.pizzaObj.name}</h3>
+                <p>{props.pizzaObj.ingredients}</p>
+                <span>{props.pizzaObj.price + 3}</span>
+            </div>
         </li>
     );
 }
@@ -144,7 +146,6 @@ function Footer() {
         </footer>
     );
 }
-
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
